0327c4870cd7c4ec1be198cdf7d7bbf43144052c
vedelopment <vedelopment@gmail.com>
2016-12-01 06:44:51 -0800

    adds 1-9, still working on bonus 10

--- functions.js
+++ functions.js
@@ -1,12 +1,11 @@
 function combineWords(word1, word2) {
-    // TODO: Place your code here
     return (word1 + word2);
 }
 
 var result = combineWords('dog', 'house');
 console.log(result);
-// displays 'doghouse'
 
+////////////////////////////////////////////////////////////////////////////
 function repeatPhrase(phrase, n) {
 	for(var i = 0; i < n; i++) {
 		console.log(phrase);
@@ -14,15 +13,9 @@ function repeatPhrase(phrase, n) {
 }
 
 repeatPhrase("Hello", 5);
-// displays
-// Hello
-// Hello
-// Hello
-// Hello
-// Hello
 
+////////////////////////////////////////////////////////////////////////////
 function toTheNthPower(number, power) {
-    // TODO: Place your code here
     result = 1;
     for(i=0; i<power; i++) {
     result *= number;
@@ -33,22 +26,20 @@ function toTheNthPower(number, power) {
 var result = toTheNthPower(4, 5);
 console.log(result);
 
+////////////////////////////////////////////////////////////////////////////
 function areaOfACircle(radius) {
-    // TODO: Place your code here
     //pi*r*r
     for(i=0; i<1; i++) {
         radius *= radius;
-        //console.log(radius);
     }
     return radius * Math.PI;
 }
 
 var result = areaOfACircle(2);
 console.log(result);
-// displays approximately 12.57
 
+////////////////////////////////////////////////////////////////////////////
 function pythagoreanTheorem(a, b) {
-    // TODO: Place your code here
     var twoSidesMath = (a*a)+(b*b);
     result = Math.sqrt(twoSidesMath);
     return result;
@@ -56,10 +47,9 @@ function pythagoreanTheorem(a, b) {
 
 var result = pythagoreanTheorem(3, 4);
 console.log(result);
-// should display 5;
 
+////////////////////////////////////////////////////////////////////////////
 function isXEvenlyDivisibleByY(x, y) {
-    // TODO: Place your code here
     if (x%y==0) {
         result = true;
     } else {
@@ -70,25 +60,57 @@ function isXEvenlyDivisibleByY(x, y) {
 
 var result = isXEvenlyDivisibleByY(99, 3);
 console.log(result);
-// displays true
 
-//THIS ONE IS NOT FINISHED!!
+////////////////////////////////////////////////////////////////////////////
 function countVowels(word) {
+	  word = word.toLowerCase();
     var wordLetters = word.split('');
-    console.log(wordLetters);
     var vowels = ["a", "e", "i", "o", "u", "y"];
-    for(n=0; n<=wordLetters.lenth; n++) {
-        var currentLetter = wordLetters[n];
-        var currentVowel = vowels[n];
-        if(currentLetter==currentVowel) {
-            return result = result + 1;
-        }
-        else {
-            return result = "You word has no vowels."
-        }
+    console.log(wordLetters);
+    console.log(vowels);
+    var howMany = 0;
+	for(var i = 0; i < word.length; i++) {
+		if( wordLetters[i] === vowels[0] || wordLetters[i] === vowels[1] || wordLetters[i] === vowels[2] || wordLetters[i] === vowels[3] ||  wordLetters[i] === vowels[4] ||  wordLetters[i] === vowels[5]){
+			howMany++;
+		}
+	}
+	return howMany;
+}
+var result = countVowels("Stealing");
+console.log(result);
+
+////////////////////////////////////////////////////////////////////////////
+function findWdi(arr){
+     for(i=0; i<arr.length; i++) {
+         if(arr[i]==='wdi') {
+             return true;
+         }
+     } return false;
+ }
+//findWdi([9,'Bart Simpson', true, 'wdi']) // returns true
+findWdi(['a','b','c']) // returns false
+
+////////////////////////////////////////////////////////////////////////////
+function printTriangle(length) {
+    for(i=1; i<=length; i++) {
+        var asterisk = "*";
+        var tree = asterisk.repeat(i)
+        console.log(tree);
     }
 }
 
-var result = countVowels("atealing");
-console.log(result);
-// displays 3
+printTriangle(5);
+
+////////////////////////////////////////////////////////////////////////////
+//function printPyramid(length) {
+    function printTriangle(length) {
+    for(i=1; i<=length; i++) {
+        var asterisk = " * ";
+        var space = " ";
+        var tree = space.repeat(i) + asterisk.repeat(i);
+        console.log(tree);
+    }
+  }
+//}
+
+printTriangle(10);
